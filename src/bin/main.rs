0f9b63f4d51a158a79d4d7a0ddd1bf14242a5c5d
louhc <louhancheng@outlook.com>
2026-02-05 21:45:00 +0800

    Add WHIR per-iteration timings and standard metrics

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::Instant};
+use std::{sync::Arc, time::{Duration, Instant}};
 
 use ark_ff::{FftField, Field};
 use ark_serialize::CanonicalSerialize;
@@ -32,6 +32,9 @@ struct Args {
     #[arg(short = 'p', long)]
     pow_bits: Option<usize>,
 
+    #[arg(short = 'm', long, default_value = "1")]
+    iterations: usize,
+
     #[arg(short = 'd', long, default_value = "20")]
     num_variables: usize,
 
@@ -44,7 +47,7 @@ struct Args {
     #[arg(short = 'r', long, default_value = "1")]
     rate: usize,
 
-    #[arg(long = "reps", default_value = "1000")]
+    #[arg(long = "reps", default_value = "1")]
     verifier_repetitions: usize,
 
     #[arg(short = 'i', long = "initfold", default_value = "4")]
@@ -119,7 +122,8 @@ fn run_whir_as_ldt<F>(
     let pow_bits = args.pow_bits.unwrap();
     let num_variables = args.num_variables;
     let starting_rate = args.rate;
-    let reps = args.verifier_repetitions;
+    let iterations = args.iterations;
+    let reps = iterations;
     let first_round_folding_factor = args.first_round_folding_factor;
     let folding_factor = args.folding_factor;
     let soundness_type = args.soundness_type;
@@ -158,7 +162,6 @@ fn run_whir_as_ldt<F>(
         .session(session!("Example at {}:{}", file!(), line!()))
         .instance(&Empty);
 
-    let mut prover_state = ProverState::from(ds.std_prover());
 
     println!("=========================================");
     println!("Whir (LDT) 🌪️");
@@ -174,21 +177,43 @@ fn run_whir_as_ldt<F>(
             .collect(),
     );
 
-    let whir_prover_time = Instant::now();
-
     let committer = CommitmentWriter::new(params.clone());
-    let witness = committer.commit(&mut prover_state, &polynomial);
-
     let prover = Prover::new(params.clone());
-
     let statement = Statement::new(num_variables);
-    prover.prove(&mut prover_state, statement.clone(), witness);
 
-    // Serialize proof
-    let proof = prover_state.proof();
+    let mut commit_times = Vec::with_capacity(iterations);
+    let mut open_times = Vec::with_capacity(iterations);
+    let mut proof_opt = None;
+    for i in 0..iterations {
+        let mut prover_state = ProverState::from(ds.std_prover());
+
+        let commit_start = Instant::now();
+        let witness = committer.commit(&mut prover_state, &polynomial);
+        let commit_ms = commit_start.elapsed().as_secs_f64() * 1000.0;
+        println!("ITER_{}_COMMIT_MS: {:.3}", i + 1, commit_ms);
+        commit_times.push(commit_ms);
+
+        let open_start = Instant::now();
+        prover.prove(&mut prover_state, statement.clone(), witness);
+        let open_ms = open_start.elapsed().as_secs_f64() * 1000.0;
+        println!("ITER_{}_OPEN_MS: {:.3}", i + 1, open_ms);
+        open_times.push(open_ms);
+
+        proof_opt = Some(prover_state.proof());
+    }
+
+    let proof = proof_opt.unwrap();
     let proof_size = proof.narg_string.len() + proof.hints.len();
-    println!("Prover time: {:.1?}", whir_prover_time.elapsed());
+    let avg = |vals: &[f64]| vals.iter().sum::<f64>() / vals.len() as f64;
+    let commit_ms = avg(&commit_times);
+    let open_ms = avg(&open_times);
+    let prover_ms = commit_ms + open_ms;
+    println!("Prover time: {:.1?}", Duration::from_secs_f64(prover_ms / 1000.0));
     println!("Proof size: {:.1} KiB", proof_size as f64 / 1024.0);
+    println!("COMMIT_TIME_MS: {:.3}", commit_ms);
+    println!("OPEN_TIME_MS: {:.3}", open_ms);
+    println!("PROVER_TIME_MS: {:.3}", prover_ms);
+    println!("PROOF_SIZE_KB: {:.3}", proof_size as f64 / 1024.0);
 
     // Just not to count that initial inversion (which could be precomputed)
     let commitment_reader = CommitmentReader::new(&params);
@@ -207,8 +232,11 @@ fn run_whir_as_ldt<F>(
             .verify(&mut verifier_state, &parsed_commitment, &statement)
             .unwrap();
     }
-    dbg!(whir_verifier_time.elapsed() / reps as u32);
+    let verify_avg = whir_verifier_time.elapsed() / reps as u32;
+    let verify_ms = verify_avg.as_secs_f64() * 1000.0;
+    dbg!(verify_avg);
     dbg!(HASH_COUNTER.get() as f64 / reps as f64);
+    println!("VERIFY_TIME_MS: {:.3}", verify_ms);
 }
 
 #[allow(clippy::too_many_lines)]
@@ -229,7 +257,8 @@ fn run_whir_pcs<F>(
     let pow_bits = args.pow_bits.unwrap();
     let num_variables = args.num_variables;
     let starting_rate = args.rate;
-    let reps = args.verifier_repetitions;
+    let iterations = args.iterations;
+    let reps = iterations;
     let first_round_folding_factor = args.first_round_folding_factor;
     let folding_factor = args.folding_factor;
     let soundness_type = args.soundness_type;
@@ -269,9 +298,6 @@ fn run_whir_pcs<F>(
     let ds = domain_separator!("🌪️")
         .session(session!("Example at {}:{}", file!(), line!()))
         .instance(&Empty);
-
-    let mut prover_state = ProverState::from(ds.std_prover());
-
     println!("=========================================");
     println!("Whir (PCS) 🌪️");
     println!("Field: {:?} and hash: {:?}", args.field, args.hash);
@@ -285,13 +311,7 @@ fn run_whir_pcs<F>(
             .map(<F as Field>::BasePrimeField::from)
             .collect(),
     );
-    let whir_prover_time = Instant::now();
-
-    let committer = CommitmentWriter::new(params.clone());
-    let witness = committer.commit(&mut prover_state, &polynomial);
-
     let mut statement: Statement<F> = Statement::<F>::new(num_variables);
-
     // Evaluation constraint
     let points: Vec<_> = (0..num_evaluations)
         .map(|x| MultilinearPoint(vec![F::from(x as u64); num_variables]))
@@ -315,16 +335,41 @@ fn run_whir_pcs<F>(
         statement.add_constraint(linear_claim_weight, sum);
     }
 
+    let committer = CommitmentWriter::new(params.clone());
     let prover = Prover::new(params.clone());
+    let mut commit_times = Vec::with_capacity(iterations);
+    let mut open_times = Vec::with_capacity(iterations);
+    let mut proof_opt = None;
+    for i in 0..iterations {
+        let mut prover_state = ProverState::from(ds.std_prover());
 
-    prover.prove(&mut prover_state, statement.clone(), witness);
+        let commit_start = Instant::now();
+        let witness = committer.commit(&mut prover_state, &polynomial);
+        let commit_ms = commit_start.elapsed().as_secs_f64() * 1000.0;
+        println!("ITER_{}_COMMIT_MS: {:.3}", i + 1, commit_ms);
+        commit_times.push(commit_ms);
 
-    let proof = prover_state.proof();
-    println!("Prover time: {:.1?}", whir_prover_time.elapsed());
-    println!(
-        "Proof size: {:.1} KiB",
-        (proof.narg_string.len() + proof.hints.len()) as f64 / 1024.0
-    );
+        let open_start = Instant::now();
+        prover.prove(&mut prover_state, statement.clone(), witness);
+        let open_ms = open_start.elapsed().as_secs_f64() * 1000.0;
+        println!("ITER_{}_OPEN_MS: {:.3}", i + 1, open_ms);
+        open_times.push(open_ms);
+
+        proof_opt = Some(prover_state.proof());
+    }
+
+    let proof = proof_opt.unwrap();
+    let avg = |vals: &[f64]| vals.iter().sum::<f64>() / vals.len() as f64;
+    let commit_ms = avg(&commit_times);
+    let open_ms = avg(&open_times);
+    let prover_ms = commit_ms + open_ms;
+    let proof_kb = (proof.narg_string.len() + proof.hints.len()) as f64 / 1024.0;
+    println!("Prover time: {:.1?}", Duration::from_secs_f64(prover_ms / 1000.0));
+    println!("Proof size: {:.1} KiB", proof_kb);
+    println!("COMMIT_TIME_MS: {:.3}", commit_ms);
+    println!("OPEN_TIME_MS: {:.3}", open_ms);
+    println!("PROVER_TIME_MS: {:.3}", prover_ms);
+    println!("PROOF_SIZE_KB: {:.3}", proof_kb);
 
     // Just not to count that initial inversion (which could be precomputed)
     let commitment_reader = CommitmentReader::new(&params);
@@ -343,10 +388,13 @@ fn run_whir_pcs<F>(
             .verify(&mut verifier_state, &parsed_commitment, &statement)
             .unwrap();
     }
+    let verify_avg = whir_verifier_time.elapsed() / reps as u32;
+    let verify_ms = verify_avg.as_secs_f64() * 1000.0;
     println!(
         "Verifier time: {:.1?}",
-        whir_verifier_time.elapsed() / reps as u32
+        verify_avg
     );
+    println!("VERIFY_TIME_MS: {:.3}", verify_ms);
     println!(
         "Average hashes: {:.1}k",
         (HASH_COUNTER.get() as f64 / reps as f64) / 1000.0
